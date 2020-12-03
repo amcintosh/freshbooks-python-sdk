b95c1c7936a807b782354ee49e7de72815391d3f
Andrew McIntosh <amcintosh@freshbooks.com>
2020-12-03 15:02:04 -0500

    💚 Disable stickler fixers

--- freshbooks/api/resource.py
+++ freshbooks/api/resource.py
@@ -36,8 +36,7 @@ class Resource:
             retry = Retry(
                 total=self.API_RETRIES,
                 backoff_factor=0.3,
-                allowed_methods=["HEAD", "GET", "PUT",
-                                 "DELETE", "OPTIONS", "TRACE"],  # type: ignore
+                allowed_methods=["HEAD", "GET", "PUT", "DELETE", "OPTIONS", "TRACE"],  # type: ignore
                 status_forcelist=[400, 408, 429, 500, 502, 503, 504],
             )
             adapter = HTTPAdapter(max_retries=retry)
